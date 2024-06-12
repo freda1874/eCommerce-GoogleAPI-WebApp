1ffa27f4e5d0b67875969528137564dfa7d86335
Rui Wang <wang0901@algonquinlive.com>
2024-06-11 20:30:13 -0400

    Create login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,13 @@ import Navbar from './components/navbar';
 import Contact from './Pages/Contact';
 import Blog from './Pages/Blog';
 import AboutUs from './Pages/AboutUs';
+import Login from './Pages/Login';  
+import { UserProvider } from './contexts/user';
 function App() {
  
   return (
     <div className="App">
+      <UserProvider>
       <BrowserRouter>
         <Navbar />
         <div className='pages'>
@@ -20,9 +23,11 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/AboutUs" element={<AboutUs />} />
+            <Route path="/profile" element={<Login />} />  
           </Routes>
         </div>
       </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
