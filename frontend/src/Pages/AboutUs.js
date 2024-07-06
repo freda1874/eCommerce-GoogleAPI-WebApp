8b189ca9fc7994c334a011626a3b7ebb9ce0f43f
Rui Wang <wang0901@algonquinlive.com>
2024-07-06 19:33:26 -0400

    Optimize the About Us interface

--- frontend/src/Pages/AboutUs.js
+++ frontend/src/Pages/AboutUs.js
@@ -4,17 +4,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 const AboutUs = () => {
 
     return (
-        <div className="home">
-            <LocationEnabler />
+        <div className="about-container">
+
             <div className="about-para">
-      <h2>About Shopful</h2>
-      <p>Welcome to Shopful, the revolutionary search engine designed exclusively for shopaholics! Our cutting-edge platform is your ultimate 
-        destination for seamless shopping exploration, making the quest for your dream products a breeze. With a vast database of items from countless 
-        online retailers, Shopful brings you the latest trends and hottest deals right to your fingertips. Unleash the power of smart searching, 
-        filtering, and price comparisons to discover hidden gems and snag the best bargains in a single click. Whether you're hunting for fashion, gadgets, 
-        home essentials, or anything in between, let Shopful be your trusty shopping companion and embark on a journey of endless discoveries today! 
-        Happy shopping!</p>
-    </div>
+                <p className="highlight">
+                Welcome to Shopful
+                </p>
+                <p>The revolutionary search engine designed exclusively for shopaholics!</p>
+                <p>Our cutting-edge platform is your ultimate destination for seamless shopping exploration, making the quest for your dream products a breeze. </p>
+                <p>With a vast database of items from countless online retailers, Shopful brings you the latest trends and hottest deals right to your fingertips. Unleash the power of smart searching, 
+                    filtering, and price comparisons to discover hidden gems and snag the best bargains in a single click. </p>
+                <p>Whether you're hunting for fashion, gadgets, home essentials, or anything in between, let Shopful be your trusty shopping companion and embark on a journey of endless discoveries today! </p>
+                <p>Happy shopping!</p>
+            </div>
+            <div className="location-container">
+                <LocationEnabler />
+            </div>
         </div>
     )
     
